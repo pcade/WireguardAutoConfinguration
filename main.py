c9f86e9f9260f1c2cefae4f365b48471327eb0a6
gpahomov <pahomovgrigorii@gmail.com>
2025-02-03 23:40:04 +0300

    поправил main

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from utils.utils import *
 from modules.key_manager import *
 from modules.ip_manager import *
-
-CLIENT_NAME = 'test'
-
-
 from modules.key_manager import *
-#private_key = create_private_keys(CLIENT_NAME).decode().strip()
-#public_key = create_pub_keys(private_key, CLIENT_NAME).decode().strip()
-#
-#
 from modules.ip_manager import *
-#ip_address = increment_ip(get_last_allowed_ip(WG0+CONF))
-#
 from modules.config_manager import *
-#append_client_to_conf(CLIENT_NAME, 'x', ip_address, private_key)
-#append_client_to_conf(WG0, 'a', ip_address, public_key)
-#
 from modules.qr_generator import qr_main
-#qr_main(WORK_DIR+CLIENT_NAME+CONF)
-#
 import modules.daemon_reload
-#modules.daemon_reload.main
-
 from modules.argparser import parse_args
 
 
@@ -40,7 +23,7 @@ def main():
     append_client_to_conf(WG0, 'a', ip_address, public_key)
     qr_main(WORK_DIR+CLIENT_NAME+CONF)
 #    modules.daemon_reload.main
-#
+
 if __name__ == "__main__":
     main()
 #print(f"ip: {args.ip}, comment: {args.comment}, clinet: {CLIENT_NAME}")
